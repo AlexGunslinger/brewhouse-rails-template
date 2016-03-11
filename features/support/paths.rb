26ea60a206a985ef5075259ff2e54e76793f0400
Alex Taylor <alexmctaylorpants@gmail.com>
2016-03-10 16:37:22 -0800

    Refactor path helper to prevent 'page' suffix from being included in path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -7,8 +7,9 @@ module NavigationHelpers
       '/'
     else
       begin
-        page_name =~ /(the|my) (.*) page/
+        page_name =~ /(the|my) (.*)( page)?/
         path_components = $2.split(/\s+/)
+        path_components.pop() if path_components.last == "page" # ensures 'page' will never be appended to the path
         self.send(path_components.push('path').join('_'))
       rescue
         raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
